fa770ce97af11888d42d215c73eb5b9af2d69a8e
Sanchit Gupta <sanchit13088@iiitd.ac.in>
2021-01-15 20:59:39 +0530

    Update v0.3

--- js/main.js
+++ js/main.js
@@ -18,8 +18,34 @@ function hideNavbarDropDown() {
     $("#myDropdown").removeClass("show");
 }
 
+function toggleNavbarDropDown() {
+    $("#myDropdown").toggleClass("show");
+}
+
+function toggleNavbar() {
+    $(".tabs").toggleClass("show");
+}
+
+function setEventsForNavbar() {
+    if (window.innerWidth <= 1000) {
+        $(".dropdown-tab").off("mouseout");
+        $(".dropdown-tab").off("mouseover");
+
+        $(".dropdown-tab").on("click", toggleNavbarDropDown);
+    } else {
+        $(".dropdown-tab").on("mouseout", hideNavbarDropDown);
+        $(".dropdown-tab").on("mouseover", showNavbarDropDown);
+
+        $(".dropdown-tab").off("click");
+    }
+}
+
 function initialize() {
     tippy('[data-tippy-content]')
+    $(".menu-btn").on("click", toggleNavbar);
+    setEventsForNavbar()
+
+    $(window).resize(setEventsForNavbar);
 }
 
 window.onload = initialize;
